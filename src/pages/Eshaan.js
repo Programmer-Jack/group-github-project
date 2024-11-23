c748abf78be131c791ded2540d64be8546d4bdd5
Eshaan Murali <109836075+eshaanmurali@users.noreply.github.com>
2024-11-22 18:16:22 -0700

    Update Eshaan.js

--- src/pages/Eshaan.js
+++ src/pages/Eshaan.js
@@ -1,7 +1,58 @@
 export default function eshaan() {
     return (
     <div>
-        <p>Fill this in</p>
+        <title>Hello, I am Eshaan Murali</title>
+            <main>
+                <h1>
+                    // Eshaan Murali
+                </h1>
+                <p>
+                    This page is meant to give you a brief idea about who I am
+                </p>
+                <p>
+                    I was born in 2005, and grew up in California. All my life I have been surrounded by technology which influenced my interest in to studying computer science.
+                </p>
+                <p>
+                   I have been extremely interested in computer science for a long time due to my many encounters with computer science. During my time doing computer science, I have worked on many projects and am looking to do more work on AI and machine learning. So far the programming languages that I know are Python, Java, C, and C++.
+                </p>
+                <p>
+                    My interests outside of computer scinece consist of math and sports. I am a huge fan of analysis and looking at sports statistics, especially for sports such as football and basketball. I am hoping to combine my interests using data science and python libraries in order to make predictive analysis for sports. 
+                </p>
+                
+                
+                <h2>
+                    <strong>// Future</strong>
+                </h2>
+                <p>
+                    In the future, I want to work in software development and hopefully progress into doing AI. I hope to work in a tech company where I can have access to using AI tools so that I could hopefully design algorithm to do things that I am interested in. 
+                </p>
+                <h2>
+                    <strong>// Contact Information</strong>
+                </h2>
+                <p>
+                    LinkedIn — Eshaan Murali
+                </p>
+                <p>
+                    GitHub — eshaanmurali
+                </p>
+                <p>
+                    Instagram — eshaan.murali
+                </p>
+                
+            </main>
+
+            <style>{`
+            main {
+                padding: 5rem 0;
+            }
+            `}</style>
+
+            <style jsx global>{`
+                html,
+                body {
+                    font-family: -arial, sans-serif;
+                }
+            `}</style>
     </div>
     );
 }
